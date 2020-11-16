244486583bb6811c27812c9a284fcea6543248e5
Safei Ashraf <Safei.ashraf@gmail.com>
2020-11-16 19:25:14 +0200

    adding api info and env

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,12 +1,14 @@
+//const apiKey = 'b4a8479340811ab1df60bf24f9729c91';
+
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(`Your API key is ${process.env.API_KEY}`);
+
+
 var path = require('path')
 const express = require('express')
+var cors = require('cors')
+const app = express()
 const bodyParser = require('body-parser');
 const mockAPIResponse = require('./mockAPI.js')
 
-const app = express()
+
 //Express middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,14 +20,17 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
+//API Info:
+const application_key =  process.env.API_KEY;
+
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
