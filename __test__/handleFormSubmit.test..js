05e0ec31d3a0008e87426bc120ed53c2609e5448
Safei Ashraf <Safei.ashraf@gmail.com>
2020-12-06 02:52:45 +0200

    update handle form tests

---
+++ __test__/handleFormSubmit.test..js
@@ -0,0 +1,10 @@
+import { handleFormSubmit } from '../src/client/index';
+
+describe('test "handleFormSubmit()" exists', () => {
+
+    test('should return true', () => {
+
+        expect(handleFormSubmit).toBeDefined();
+    });
+    
+});
