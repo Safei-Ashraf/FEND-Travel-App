a0c79db04c37320f9b0af5cb297aefe58657bbc3
Safei Ashraf <Safei.ashraf@gmail.com>
2020-12-07 21:25:59 +0200

    updating server files

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 var fetch = require('node-fetch')
 
-console.log(`Your API key is ${process.env.API_KEY}`);
-
 
 var path = require('path')
 const express = require('express')
 var cors = require('cors')
 const app = express()
 const bodyParser = require('body-parser');
-const mockAPIResponse = require('./mockAPI.js')
+const { trips_data } = require('../client/js/app.js');
 const PORT = process.env.PORT || 8080;
 
 
@@ -24,50 +22,76 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 
-//API Info: /// TO BE REMOVED
-const application_key =  process.env.API_KEY;
+// //API Info: /// TO BE REMOVED
+// const application_key =  process.env.API_KEY;
+
+//request Example:
+// const getSentiment = async(req,r)=>{
+
+//     let formText = req.body.text;
+//     const baseURL ="https://api.meaningcloud.com/sentiment-2.1?";
+  
+//     const res = await  fetch(`${baseURL}key=${application_key}&url=${formText}&lang=auto`);
+//     try {
+  
+//       const data = await res.json();
+//       console.log(data)
+  
+//       r.send(data);
+//     }  catch(error) {
+//       console.log("error", error);
+//     }
+//   }
+  
+  // app.post('/sentiment', function(req,res){
+  //   getSentiment(req,res);
+  // })
+
 
 //request:
-const getSentiment = async(req,r)=>{
+const getWeather = async (req,r)=>{
 
-    let formText = req.body.text;
-    const baseURL ="https://api.meaningcloud.com/sentiment-2.1?";
-  
-    const res = await  fetch(`${baseURL}key=${application_key}&url=${formText}&lang=auto`);
-    try {
-  
-      const data = await res.json();
-      console.log(data)
-  
-      r.send(data);
-    }  catch(error) {
-      console.log("error", error);
-    }
-  }
-  
-  app.post('/sentiment', function(req,res){
-    getSentiment(req,res);
-  })
+    const getApiResponse = await fetch(req.body.url)
+    .then(resp => resp.json())
+    .then( info =>{
+      console.log(`here from inside the Server POST`);
+      trips_data.temp = info.data[0].temp;
+      trips_data.weatherDescription = info.data[0].weatherDescription;
+      console.log(req.body);
+      console.log(trips_data);
+    })
+}
+
+
+
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
+
+
+//GET Route Setup:
+//Sends back the Project Data Object:
+app.get('/add',((req,res)=>{
+  console.log(`here from inside the Server GET`);
+  res.send(trips_data);
+  console.log(trips_data);
+}))
+
+
+
+
+
+//POST Route Setup:
+app.post('/add', getWeather);
+
+// function addWeather (req,res){
+
+
+// };
 
 // designates what port the app will listen to for incoming requests
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!!`)
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
-
-// app.post('/', function(req,res){
-//     let data = req.body;
-//     console.log(`This is the new POST body SERVER SIDE: ${data}`);
-//     projectData['confidence'] = data.confidence;
-//     projectData['agreement'] = data.agreement;
-//     projectData['score_tag'] = data.score_tag;
-//     res.send(projectData)
-//     console.log(`This is projectData result after post ${projectData}`);
-// });
--- src/server/mockAPI.js
+++
@@ -1,7 +0,0 @@
-let json = {
-    'title': 'test json response',
-    'message': 'this is a message from MOCK API',
-    'time': 'now'
-}
-
-module.exports = json
