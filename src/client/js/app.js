c98d7555f06eae7a6a25eace256fd6d20238616e
Safei Ashraf <Safei.ashraf@gmail.com>
2020-12-07 22:22:35 +0200

    adding server methods

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -123,3 +123,23 @@ export const showElem = (elem)=>{
     elem.style.display = 'block';
     return;
 }
+
+
+//POST Data to Server:
+async function postData (url, data = {}){
+    const options = {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+        'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+}
+const response = await fetch(url, options);
+//const newData = await response.json();
+}
